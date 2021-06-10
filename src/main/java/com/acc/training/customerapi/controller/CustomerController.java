88902e35007f45a8509b78bc2a557a709f046b88
Ossé Laborde <oml007@live.com>
2021-06-09 20:06:44 -0400

    First commit

---
+++ src/main/java/com/acc/training/customerapi/controller/CustomerController.java
@@ -0,0 +1,37 @@
+package com.acc.training.customerapi.controller;
+
+import javax.validation.Valid;
+
+import com.acc.training.customerapi.api.CustomerApi;
+import com.acc.training.customerapi.model.Customer;
+import com.acc.training.customerapi.service.CustomerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController implements CustomerApi {
+
+    @Autowired
+    private CustomerService service;
+
+    @Override
+    public ResponseEntity<Customer> createCustomer(@Valid Customer body) {
+        Customer response = service.createCustomer(body);
+        //TODO: Validated customerID min length and max length 8
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Override
+    public ResponseEntity<Customer> getCustomer(String id) {
+
+        Customer response = service.getCustomer(id);
+        if(response == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+     
+}
