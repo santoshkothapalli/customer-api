05166652724d934c5a0cf6f65c95d8f05befdade
Anipso <85418927+Anipso@users.noreply.github.com>
2021-06-11 18:25:12 -0400

    added mongodb functionality

---
+++ src/main/java/com/acc/training/customerapi/domain/CustomerDomain.java
@@ -0,0 +1,60 @@
+package com.acc.training.customerapi.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "customers-training-ospina")
+public class CustomerDomain {
+    @Id
+    private String id;
+
+    private String customerId = null;
+
+    private String customerName = null;  
+
+    private String customerAddress = null;
+  
+    private Integer officeCode = null;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCustomerId() {
+        return this.customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return this.customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public Integer getOfficeCode() {
+        return this.officeCode;
+    }
+
+    public void setOfficeCode(Integer officeCode) {
+        this.officeCode = officeCode;
+    }
+
+
+}
--- src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
+++ src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
@@ -1,41 +1,15 @@
 package com.acc.training.customerapi.repository;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.acc.training.customerapi.domain.CustomerDomain;
 
-import com.acc.training.customerapi.model.Customer;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-@Repository
-public class CustomerRepository {
 
-    private static final Map<String, Customer> tempCustomerDB = new HashMap<>();
+public interface CustomerRepository extends MongoRepository<CustomerDomain, String> {
 
-    static{
-        initCustomerDB();
-    }
+    public CustomerDomain findByCustomerId(String customerId);
 
-    private static void initCustomerDB() {
-
-        Customer c1 = new Customer();
-        c1.setCustomerId("12345");
-        c1.setCustomerName("Jessica");
-        c1.setCustomerAddress("NewYork");
-        c1.setOfficeCode(630);
-
-        tempCustomerDB.put(c1.getCustomerId(), c1);
-
-    }
-
-    public Customer getCustomerDetails(String id){
-        return tempCustomerDB.get(id);
-    }
-
-    public Customer saveCustomerDetails(Customer cust){
-        tempCustomerDB.put(cust.getCustomerId(), cust);
-        return tempCustomerDB.get(cust.getCustomerId());
-
-    }
     
+
 }
