7c31d6e1c77860aa23060c4751ce02895b6a3825
deerajbs <deeraj.bs@gmail.com>
2021-06-11 16:32:09 -0400

    customer api

---
+++ src/main/java/com/acc/training/customerapi/controller/CustomerController.java
@@ -0,0 +1,42 @@
+package com.acc.training.customerapi.controller;
+
+import com.acc.training.customerapi.api.CustomerApi;
+import com.acc.training.customerapi.model.Customer;
+import com.acc.training.customerapi.service.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity; 
+
+@RestController
+public class CustomerController implements CustomerApi {
+
+    @Autowired
+    private CustomerService service;
+
+    @Override
+    public ResponseEntity<Customer> createCustomer(@Valid Customer body) {
+        Customer response = service.createCustomer(body);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Override
+    public ResponseEntity<Customer> getCustomer(String id) {
+        Customer response = new Customer();
+
+        if (id.length() >= 5 && id.length() <= 8) {
+            response = service.getCustomer(id);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        if (response == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+}
+    
+
