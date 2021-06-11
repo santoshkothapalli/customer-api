8765b5559d0b8fb43cfbb79f5e3f86ad52090c99
Graham Guthrie <grahamguthrie99@gmail.com>
2021-06-11 18:24:43 -0400

    mongo integration

--- src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
+++ src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
@@ -1,40 +1,13 @@
 package com.acc.training.customerapi.repository;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import javax.validation.Valid;
+import com.acc.training.customerapi.domain.CustomerDomain;
 
-import com.acc.training.customerapi.model.Customer;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class CustomerRepository {
-
-    private static final Map<String, Customer> tempCustomerDB = new HashMap<>(); 
-
-    static{
-        initCustomerDB();
-    }
-
-    private static void initCustomerDB() {
-        Customer c1 = new Customer(); 
-        c1.setCustomerAddress("City, St");
-        c1.setCustomerId("123");
-        c1.customerName("Graham Guthrie"); 
-        c1.setOfficeCode(123);
-        tempCustomerDB.put(c1.getCustomerId(), c1); 
-    }
-
-    public Customer saveCustomer(@Valid Customer body) {
-        tempCustomerDB.put(body.getCustomerId(), body);
-        return tempCustomerDB.get(body.getCustomerId()); 
-    }
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 
-    public Customer getCustomer(String id) {
-        return tempCustomerDB.get(id);
-    }
+public interface CustomerRepository extends MongoRepository<CustomerDomain, String>{
+
+    public CustomerDomain findByCustomerId(String customerId); 
     
 }
--- src/main/java/com/acc/training/customerapi/service/CustomerService.java
+++ src/main/java/com/acc/training/customerapi/service/CustomerService.java
@@ -2,6 +2,7 @@ package com.acc.training.customerapi.service;
 
 import javax.validation.Valid;
 
+import com.acc.training.customerapi.domain.CustomerDomain;
 import com.acc.training.customerapi.model.Customer;
 import com.acc.training.customerapi.repository.CustomerRepository;
 
@@ -15,13 +16,34 @@ public class CustomerService {
     CustomerRepository repository; 
 
     public Customer createCustomer(@Valid Customer body) {
-        return repository.saveCustomer(body);
+        CustomerDomain customerDomain = mapModelToDomain(body); 
+        return mapDomainToModel(repository.save(customerDomain));
     }
+
 
     public Customer getCustomer(String id) {
-        return repository.getCustomer(id);
+        return mapDomainToModel(repository.findByCustomerId(id));
     }
 
+    private CustomerDomain mapModelToDomain(@Valid Customer body) {
+        CustomerDomain customer = new CustomerDomain(); 
+        customer.setCustomerId(body.getCustomerId());
+        customer.setCustomerAddress(body.getCustomerAddress());
+        customer.setCustomerName(body.getCustomerName());
+        customer.setOfficeCode(body.getOfficeCode());
+        return customer;
+    }
+
+    private Customer mapDomainToModel(@Valid CustomerDomain customerDomain) {
+        Customer customer = new Customer(); 
+        customer.setCustomerId(customerDomain.getCustomerId());
+        customer.setCustomerAddress(customerDomain.getCustomerAddress());
+        customer.setCustomerName(customerDomain.getCustomerName());
+        customer.setOfficeCode(customerDomain.getOfficeCode());
+        return customer;
+    }
+
+    
     
     
 }
